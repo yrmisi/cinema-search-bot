277624661e018659c14c4c79ddeeaabff486e3b9
yrmisi <yrmisi@mail.ru>
2025-12-16 21:44:13 +0300

    add logging in file echo.py and start.py

--- src/handlers/default/start.py
+++ src/handlers/default/start.py
@@ -1,8 +1,11 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, User
 from aiogram.utils.formatting import Bold, Text
 
+from logging_config import get_logger
+
+logger = get_logger(__name__)
 router = Router()
 
 
@@ -11,6 +14,13 @@ async def command_start_handler(message: Message) -> Message:
     """
     This handler receives messages with `/start` command
     """
+    user: User | None = message.from_user
+    if user:
+        logger.info(
+            "The user (full name - %s, id - %s) pressed the command 'start'.",
+            user.full_name,
+            user.id,
+        )
     full_name: str = message.from_user.full_name if message.from_user else "user"
     entities: Text = Text("Hello ,", Bold(full_name), "!")
 
